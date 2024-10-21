f98fe4a43501071f7fba8f932cd90de817c55563
Ryan Petrunyak <pryan22@vt.edu>
2024-10-21 01:16:40 -0400

    Updated the backend to display all data in the current database. Added a new Database Connection Proof png with the data stored in the database.

--- Back/app.js
+++ Back/app.js
@@ -1,23 +1,57 @@
-var express = require("express");
-var config = require('./config.json')
-var app = express();
+const express = require("express");
+const config = require('./config.json');
+const app = express();
+const connection = require("./db");
 
-var connection = require("./db");
+app.get('/', function(req, res) {
+  const sql = `
+    SELECT CONCAT('SELECT * FROM ', table_name, ';') AS query
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE table_schema = 'football_data';
+  `;
 
-app.get('/', function(req, res){
-  let sql = "SELECT * FROM testtable";
-  connection.query(sql, function(err, results){
-    if (err) throw err;
-    res.send(`Connected to the database as id ${connection.threadId}! The database contents are as follows:\n ${JSON.stringify(results, null, 2)}`);
-  })
-})
-app.listen(config.AppConfig.port, function(){
-  console.log(`Connecting to database on port ${config.DatabaseConfig.port}`);
-  connection.connect(function(err){
-    if(err){
-      throw err;
-    } 
-  })
-})
+  connection.query(sql, function(err, results) {
+    if (err) {
+      console.error('Error fetching table queries:', err);
+      return res.status(500).send('Error fetching table queries');
+    }
+
+    let allData = [];
+
+    function executeQuery(index) {
+      if (index >= results.length) {
+        return res.send(
+          `Connected to the database as id ${connection.threadId}! The database contents are as follows:\n ${JSON.stringify(allData, null, 2)}`
+        );
+      }
+    
+      const query = results[index].query;
+      const table_name = query.split(' ')[3];
+    
+      connection.query(query, function(err, tableData) {
+        if (err) {
+          console.error('Error executing query:', query, err);
+          return res.status(500).send('Error fetching table data');
+        }
+        allData.push({ table: table_name, data: tableData });
+        executeQuery(index + 1);
+      });
+    }
+    executeQuery(0);
+  });
+});
+
+
+// Start the server and connect to the database.
+app.listen(config.AppConfig.port, function() {
+  console.log(`Server running on port ${config.AppConfig.port}`);
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      process.exit(1);
+    }
+    console.log('Connected to the database');
+  });
+});
 
 module.exports = app;
